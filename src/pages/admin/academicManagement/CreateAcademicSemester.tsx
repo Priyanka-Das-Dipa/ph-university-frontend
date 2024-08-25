72929331c496dfbf2ca9d879512b219662a2a085
Priyanka Das Dipa <priyankadipa9102@gmail.com>
2024-08-25 22:06:47 +0600

    Table form ANT Design and redux transfrom result working

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -4,35 +4,40 @@ import { Button, Col, Flex } from "antd";
 import PHSelect from "../../../components/form/PHSelect";
 import { monthOptions } from "../../../constants/globals";
 import { zodResolver } from "@hookform/resolvers/zod";
-
 import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
+import { semesterOptions } from "../../../constants/semester";
+import { useAddAcademicSemesterMutation } from "../../../redux/features/admin/academicManagement.api";
+import { toast } from "sonner";
+import { TResponse } from "../../../types/globals";
+
+const nameOptions = [
+  {
+    value: "01",
+    label: "Autumn",
+  },
+  {
+    value: "02",
+    label: "Summer",
+  },
+  {
+    value: "03",
+    label: "Fall",
+  },
+];
+
+const currentYear = new Date().getFullYear();
+const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
+  value: String(currentYear + number),
+  label: String(currentYear + number),
+}));
 
 const CreateAcademicSemester = () => {
-  const nameOptions = [
-    {
-      value: "01",
-      label: "Autumn",
-    },
-    {
-      value: "02",
-      label: "Summer",
-    },
-    {
-      value: "03",
-      label: "Fall",
-    },
-  ];
+  const [addAcademicSemester] = useAddAcademicSemesterMutation();
 
-  const currentYear = new Date().getFullYear();
-  const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
-    value: String(currentYear + number),
-    label: String(currentYear + number),
-  }));
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastedId = toast.loading("Creating...");
 
-  console.log(yearOptions);
-
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    const name = nameOptions[Number(data?.name) - 1]?.label;
+    const name = semesterOptions[Number(data?.name) - 1]?.label;
 
     const semesterData = {
       name,
@@ -42,7 +47,16 @@ const CreateAcademicSemester = () => {
       endMonth: data.endMonth,
     };
 
-    console.log(semesterData);
+    try {
+      const res = (await addAcademicSemester(semesterData)) as TResponse;
+      if (res?.error) {
+        toast.error(res?.error?.data?.message, { id: toastedId });
+      } else {
+        toast.success("Semester Created Successfully", { id: toastedId });
+      }
+    } catch (error) {
+      toast.error("Something went wrong! Try again.", { id: toastedId });
+    }
   };
 
   return (
